b7402d410e784614871df255bb978b11eaaf67cc
Jarko <hassan.jarko@multibankfx.com>
2023-11-27 21:19:39 +0400

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,171 @@
-import Image from 'next/image'
+"use client";
+
+import { useFormik } from "formik";
+import countries from "../../public/data/countries.json";
+import * as Yup from "yup";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      country: "",
+      terms: "",
+    },
+    validationSchema: Yup.object({
+      first_name: Yup.string()
+        .required("first name is required")
+        .min(3, "at least 3 letters"),
+      last_name: Yup.string()
+        .required("Last name is required")
+        .min(3,"at least 3 letters"),
+      email: Yup.string()
+        .email("Invalid Email")
+        .required("Email Address is required"),
+        terms: Yup.array().required("Terms of service must be checked")
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen items-center ">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white flex rounded-lg font-mono p-10 w-1/2 mx-auto"
+      >
+        <div className="mt-6">
+          <div className="pb-4">
+            <label
+              className={`block text-sm pb-2 ${
+                formik.errors.first_name ? "text-red-500" : ""
+              }`}
+              htmlFor="first_name"
+            >
+              {formik.touched.first_name && formik.errors.first_name
+                ? formik.errors.first_name
+                : ""}
+            </label>
+
+            <input
+              type="text"
+              name="first_name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.first_name}
+              placeholder="Enter your first name"
+              className="w-[100%] border-2 border-gray-500 p-2 rounded-md focus:border-2 focus:border-teal-500 focus:ring-teal-500"
             />
-          </a>
+          </div>
+          <div className="pb-4">
+            <label
+              className={`block text-sm pb-2 ${
+                formik.errors.last_name ? "text-red-500" : ""
+              }`}
+              htmlFor="last_name"
+            >
+              {formik.touched.last_name && formik.errors.last_name
+                ? formik.errors.last_name
+                : ""}
+            </label>
+            <input
+              type="text"
+              name="last_name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.last_name}
+              placeholder="Enter your last name"
+              className="w-[100%] border-2 border-gray-500 p-2 rounded-md focus:border-2 focus:border-teal-500 focus:ring-teal-500"
+            />
+          </div>
+          <div className="pb-4">
+            <label
+              className={`block text-sm pb-2 ${
+                formik.errors.last_name ? "text-red-500" : ""
+              }`}
+              htmlFor="email"
+            >
+              {formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : ""}
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              placeholder="Enter your Email Address"
+              className="w-[100%] border-2 border-gray-500 p-2 rounded-md focus:border-2 focus:border-teal-500 focus:ring-teal-500"
+            />
+          </div>
+          <div className="pb-4">
+            <label
+              className={`block text-sm pb-2 ${
+                formik.errors.country ? "text-red-500" : ""
+              }`}
+              htmlFor="country"
+            >
+              {formik.touched.country && formik.errors.country
+                ? formik.errors.country
+                : ""}
+            </label>
+            <select
+              value={formik.values.country}
+              id="country"
+              name="country"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className="border-2 border-gray-500 p-2 rounded-md focus:border-2 focus:border-teal-500 focus:ring-teal-500"
+            >
+              {countries.map((el, index) => {
+                return (
+                  <option key={index} value={el.code}>
+                    {el.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="pb-4">
+            <label
+              className={`block text-sm pb-2 ${
+                formik.touched.terms && formik.errors.terms ? "text-red-500" : ""
+              }`}
+              htmlFor="terms"
+            >
+              {formik.touched.terms && formik.errors.terms
+                ? formik.errors.terms
+                : "Terms of service"}
+            </label>
+            <div className="flex items-center gap-2">
+              <input
+                type="checkbox"
+                name="terms"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                id="terms"
+                value="checked"
+                className="h-5 w-5 text-teal-500"
+              />
+              <p>
+                I agree to the terms and Service that my data will be taken and
+                sold
+              </p>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-teal-500 font-bold text-sm text-white py-3 px-4 mt-6 rounded-lg"
+          >
+            Submit
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </form>
     </main>
-  )
+  );
 }
